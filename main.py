b6dbecff73a10f111a42827a5e25d195c5aa6046
Rex6961 <rex6961@gmail.com>
2023-06-28 20:12:38 -0300

    Added to use random colores for output text

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang.builder import Builder
-# from kivy.uix.scatter import Scatter
-# from kivy.uix.label import Label
-# from kivy.uix.floatlayout import FloatLayout
-# from kivy.uix.textinput import TextInput
 
+# import random for to use random colores
 
 #if the file name is not 'MyApp.kv'
 # import library 'kivy.lang.builder' with the name of your file.
@@ -15,29 +12,16 @@ Builder.load_file('my.kv')
 
 class ScatterTextWidget(BoxLayout):
     pass
+    # to use code for random colores or another to use code in my.kv file
+    # def change_label_colour(self, *args):
+    #     a = random.random()
+    #     colour = [random.random() for i in range(3)] + [a + 0.3 if a < 0.4 else a]
+    #     label = self.ids['my_label']
+    #     label.color = colour
 
 
 class TutorialApp(App):
 
-    # def build(self):
-    #     b = BoxLayout(orientation='vertical')
-    #     t = TextInput(font_size=50,
-    #                   size_hint_y=None,
-    #                   height=75,
-    #                   text='Hello, world!')
-    #     f = FloatLayout()
-    #     s = Scatter()
-    #
-    #     l = Label(text='Hello, world!',
-    #                   font_size=50)
-    #     t.bind(text=l.setter('text'))
-    #
-    #     f.add_widget(s)
-    #     s.add_widget(l)
-    #
-    #     b.add_widget(t)
-    #     b.add_widget(f)
-    #     return b
     def build(self):
         return ScatterTextWidget()
 
