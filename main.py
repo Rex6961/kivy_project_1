f3104e150aa065e7a3929b2dc204c5a7c082edd9
Rex6961 <rex6961@gmail.com>
2023-06-28 16:45:27 -0300

    Simple scatter lable app 'Hello, world'

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from kivy.app import App
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
 
 
-# Press the green button in the gutter to run the script.
+class TutorialApp(App):
+
+    def build(self):
+        f = FloatLayout()
+        s = Scatter()
+
+        l = Label(text='Hello, world!',
+                      font_size=50)
+        f.add_widget(s)
+        s.add_widget(l)
+        return f
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    TutorialApp().run()
