6f30cb0470f868b75b955a57b6f4e9c9171c1afc
Rex6961 <rex6961@gmail.com>
2023-06-28 17:51:28 -0300

    Added box layout and text input. The input text is in sync with the floating layout. Also added library buildozer for build app for android.

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ from kivy.app import App
 from kivy.uix.scatter import Scatter
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
 
 
 class TutorialApp(App):
 
     def build(self):
+        b = BoxLayout(orientation='vertical')
+        t = TextInput(font_size=50,
+                      size_hint_y=None,
+                      height=75,
+                      text='Hello, world!')
         f = FloatLayout()
         s = Scatter()
 
         l = Label(text='Hello, world!',
                       font_size=50)
+        t.bind(text=l.setter('text'))
+
         f.add_widget(s)
         s.add_widget(l)
-        return f
+
+        b.add_widget(t)
+        b.add_widget(f)
+        return b
 
 
 if __name__ == '__main__':
