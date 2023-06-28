4e5a93008f7781de5f60eec18fdd48a30d9c86f9
Rex6961 <rex6961@gmail.com>
2023-06-28 19:03:36 -0300

    Created a new file my.kv to describe methods in the kivy language. This method facilitates the structure of the code. In the kivy language, the description of methods and variables is carried out in python.

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 from kivy.app import App
-from kivy.uix.scatter import Scatter
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+from kivy.lang.builder import Builder
+# from kivy.uix.scatter import Scatter
+# from kivy.uix.label import Label
+# from kivy.uix.floatlayout import FloatLayout
+# from kivy.uix.textinput import TextInput
+
+
+#if the file name is not 'MyApp.kv'
+# import library 'kivy.lang.builder' with the name of your file.
+# Example Builder.load_file('name.kv')
+Builder.load_file('my.kv')
+
+
+class ScatterTextWidget(BoxLayout):
+    pass
 
 
 class TutorialApp(App):
 
+    # def build(self):
+    #     b = BoxLayout(orientation='vertical')
+    #     t = TextInput(font_size=50,
+    #                   size_hint_y=None,
+    #                   height=75,
+    #                   text='Hello, world!')
+    #     f = FloatLayout()
+    #     s = Scatter()
+    #
+    #     l = Label(text='Hello, world!',
+    #                   font_size=50)
+    #     t.bind(text=l.setter('text'))
+    #
+    #     f.add_widget(s)
+    #     s.add_widget(l)
+    #
+    #     b.add_widget(t)
+    #     b.add_widget(f)
+    #     return b
     def build(self):
-        b = BoxLayout(orientation='vertical')
-        t = TextInput(font_size=50,
-                      size_hint_y=None,
-                      height=75,
-                      text='Hello, world!')
-        f = FloatLayout()
-        s = Scatter()
-
-        l = Label(text='Hello, world!',
-                      font_size=50)
-        t.bind(text=l.setter('text'))
-
-        f.add_widget(s)
-        s.add_widget(l)
-
-        b.add_widget(t)
-        b.add_widget(f)
-        return b
+        return ScatterTextWidget()
 
 
 if __name__ == '__main__':
